8fa056433cd443378fcb4753749e2cdb7d786f73
yellowja <yellowja@student.21-school.ru>
2023-05-26 10:40:19 +0700

    add print_arg()

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -4,11 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+void print_arg(va_list argptr, char mas_format) {
+    switch (mas_format) {
+        case 'd':
+            printf("%d", va_arg(argptr, int));
+            break;
+        case 'c':
+            printf("%c", va_arg(argptr, int));
+            break;
+        case 's':
+            printf("%s", va_arg(argptr, char*));
+            break;
+        default:
+            break;
+    }
+}
 
 int s21_sprintf(char* buf, char* format, ...)
 {
     char mas_format[100];
-    buf =buf;
+    buf = buf;
     va_list argptr;
     va_start(argptr, format);
     int k=0;
@@ -18,12 +33,8 @@ int s21_sprintf(char* buf, char* format, ...)
             mas_format[k++] = format[i];
 
     int mas_format_len = strlen(mas_format);
-    for(int i=0; i<mas_format_len; i++)
-    {
-        int s = va_arg(argptr, int);
-        if(mas_format[i] == 'd' && (int)s == s) printf("%d", s);
-        if(mas_format[i] == 'c' && (char)s==s) printf("%d", s);
-        //if(mas_format[i] == 's' && s==s) printf("%d", s);
+    for(int i=0; i<mas_format_len; i++) {
+        print_arg(argptr, mas_format[i]);
     }
     printf("\n");
     va_end(argptr);
