9e27459aa6ac80da7d3622d5a185e68b7eda94db
yellowja <yellowja@student.21-school.ru>
2023-05-28 03:54:23 +0700

    int to str

--- src/test.c
+++ src/test.c
@@ -1,6 +1,11 @@
-#include<stdio.h>
+#include "s21_string.h"
+#include <stdio.h>
 
-int main() {
-
+int main ()
+{
+    char bas[80];
+    //sprintf(bas, "%c %c %d", '1', 2, '3');
+    s21_sprintf(bas, "%s %c %d", "one", 'N', 304);
+    printf("%s\n", bas);
     return 0;
 }
