fce5d8071c5eaede6506b99d48464883bf4d5719
Macy Gabrielle <macygabr@student.21-school.ru>
2023-05-28 04:06:52 +0700

    add base-flag, to_string

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -4,21 +4,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-void sellect_arg(char* buf,int *index, va_list argptr, char mas_format) {
-    switch (mas_format) {
+void itoa(int n, char s[])
+ {
+     int i, j, sign;
+     char c;
+     if ((sign = n) < 0)  /* record sign */
+         n = -n;          /* make n positive */
+     i = 0;
+     do {       /* generate digits in reverse order */
+         s[i++] = n % 10 + '0';   /* get next digit */
+     } while ((n /= 10) > 0);     /* delete it */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+     }
+} 
+
+void sellect_arg(char* buf,int *index, va_list argptr, char format) {
+    
+    switch (format) {
         case 'd':
-            buf[(*index)++] = va_arg(argptr, int);
+            char str1[20];
+            itoa(va_arg(argptr, int), str1);
+            buf = strcat(buf,str1);
+            (*index) +=strlen(str1);
             break;
         case 'c':
             buf[(*index)++] = va_arg(argptr, int);
             break;
         case 's':
-            char* str1 = va_arg(argptr,  char*);
-            buf = strcat(buf,str1);
-            (*index) +=strlen(str1);
+            char* str2 = va_arg(argptr,  char*);
+            buf = strcat(buf,str2);
+            (*index) +=strlen(str2);
             break;
         default:
-            // error
+            // char out[512] = "grep: ";
+            // strcat(out, optarg);
+            // perror(out);
             break;
     }
 }
@@ -27,22 +53,16 @@ void sellect_arg(char* buf,int *index, va_list argptr, char mas_format) {
 
 int s21_sprintf(char* buf, char* format, ...)
 {
-    char mas_format[100];
     va_list argptr;
     va_start(argptr, format);
-    int k=0;
     int index = 0;
-
-    for(int i=0; format[i] != '\0'; i++)
-        if(format[i] != '%')
-            mas_format[k++] = format[i];
-
-    int mas_format_len = strlen(mas_format);
-    for(int i=1; i<=mas_format_len; i++) {
-        if(mas_format[i-1] == '%')
-        sellect_arg(buf,&index, argptr, mas_format[i]);
+    buf[0] = '\0';
+    int format_len = strlen(format);
+    for(int i=0; i<format_len; i++) {
+        if(format[i] == '%')
+        sellect_arg(buf,&index, argptr, format[++i]);
         else
-        buf[(index)++] = mas_format[i];
+        buf[(index)++] = format[i];
     }
     va_end(argptr);
     return 0;
