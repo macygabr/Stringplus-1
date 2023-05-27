20fb26aaca7cc51140ef4a27c17034e8ef89239c
Macy Gabrielle <macygabr@student.21-school.ru>
2023-05-28 02:52:09 +0700

    del_struct

--- src/s21_string.h
+++ src/s21_string.h
@@ -3,12 +3,5 @@
 
 int s21_string();
 int s21_sprintf(char* buf, char* format, ...);
-//void print_arg(va_list argptr, base_mass str);
-
-typedef struct {
-    char mas_format[100];
-    int mas_format_len;
-    int type;
-} base_mass;
 
 #endif
