8db596d936cbf3484f9fbfffcc9d9394e7be480d
yellowja <yellowja@student.21-school.ru>
2023-05-28 12:07:21 +0700

    TRY CATCH?

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -4,27 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
- /* itoa:  convert n to characters in s */
- void itoa(int n, char s[])
- {
-     int i, j, sign;
-     char c;
-
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-
-     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
+/* itoa:  convert n to characters in s */
+void itoa(int n, char s[])
+{
+    int i, j, sign;
+    char c;
+    if ((sign = n) < 0)  /* record sign */
+        n = -n;          /* make n positive */
+    i = 0;
+    do {       /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';   /* get next digit */
+    } while ((n /= 10) > 0);     /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    int sLen = strlen(s)-1;
+    for (i = 0, j = sLen; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 } 
 
 void sellect_arg(char* buf,int *index, va_list argptr, char mas_format) {
--- src/test.c
+++ src/test.c
@@ -1,11 +1,24 @@
 #include "s21_string.h"
 #include <stdio.h>
+#include <setjmp.h>
 
-int main ()
-{
-    char bas[80];
-    //sprintf(bas, "%c %c %d", '1', 2, '3');
-    s21_sprintf(bas, "%s %c %d", "one", 'N', 304);
-    printf("%s\n", bas);
+#define TRY do { jmp_buf buf_state; if ( !setjmp(buf_state)) {
+#define CATCH } else {
+#define ENDTRY }} while(0)
+#define THROW longjmp(buf_state, 1)
+
+int main() {
+    char bas[80]; 
+    TRY {
+        printf("Testing Try statement\n");
+        s21_sprintf(bas, "%s %c %s\n", "one", 'N', 304);
+        printf("%s\n", bas);
+        THROW;
+        printf("Statement should not appear, as the THROW block has already thrown the exception \n");
+    }
+    CATCH {
+        printf("Got Exception \n");
+    }
+    ENDTRY;
     return 0;
 }
