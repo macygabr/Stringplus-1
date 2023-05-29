2efd9b6e7b14bb346043f04944fafd0085aaef69
Macy Gabrielle <macygabr@student.21-school.ru>
2023-05-30 00:25:11 +0700

    new test

--- src/test_my_func.c
+++ src/test_my_func.c
@@ -4,9 +4,9 @@
 int main ()
 {
     char bas[200];
-    s21_sprintf(bas, "%s %c %-12d %u %%", "one", '8', -10, 10);
+    s21_sprintf(bas, "%-4s %c %-10d %u %% %+11f % 2f", "one", '8', -10, 11, -12.1, -13.1);
     printf("pice of shit = \"%s\"\n", bas);
-    sprintf(bas, "%s %c %-12d %u %%", "one", '8', -10, 10);
+    sprintf(bas, "%-4s %c %-10d %u %% %+11f % 2f", "one", '8', -10, 11, -12.1, -13.1);
     printf("origin       = \"%s\"\n", bas);
     return 0;
 }
