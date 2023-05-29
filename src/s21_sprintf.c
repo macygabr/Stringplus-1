7e1c35f7cd2fc444415ed02ec79c0eda6d3ed82f
Macy Gabrielle <macygabr@student.21-school.ru>
2023-05-29 22:19:46 +0700

    add flag "-"

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,57 +1,92 @@
 #include "s21_string.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-void itoa(int n, char s[])
+
+void itoa(long double n, char s[], int itsFloat)
  {
-     int i, j, sign;
-     char c;
-     if ((sign = n) < 0)  /* record sign */
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
+    
+    long int i, j, sign, notWhole = -1;
+    char c;
+    if ((sign = n) < 0)  /* record sign */
+        n = -n;          /* make n positive */
+    if(itsFloat)
+    {
+        for(notWhole = 0; notWhole < 7; notWhole++) 
+            n*=10;
+    n = (long int)n;
+    if ((long int)n % 10 >= 5) {
+            n /= 10;
+            n++;
+    } else n /= 10;
+    notWhole--;
+    }
+    i = 0;
+    n = (long int)n;
+    do {
+        if(notWhole != i)
+            s[i++] = (long int)n % 10 + '0';
+        else {
+            s[i++] = '.';
+            s[i++] = (long int)n % 10 + '0';
+        }
+    } while ((long int)(n /= 10) > 0);
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
 
-void sellect_arg(char* buf,int *index, va_list argptr, char format) {
-    
+void add_spase(char* buf, write_in_buf* output, int space)
+{
+    for(int j=0; j<space; j++){
+    buf[output->index_buf_mass++] = ' ';}
+}
+double simple_pow(int base,int exp)
+{
+    if( exp ==0) return 1;
+
+    double res = base;
+    for(int i =1; i<exp; i++) res*= base;
+    return res;
+}
+void sellect_arg(char* buf, write_in_buf* output, char format) {
+
     switch (format) {
         case 'd':
             char str1[20];
-            itoa(va_arg(argptr, int), str1);
+            itoa(va_arg(output->argptr, int), str1, 0);
+            output->str_long = strlen(str1);
             buf = strcat(buf,str1);
-            (*index) +=strlen(str1);
+            output->index_buf_mass +=strlen(str1);
             break;
         case 'c':
-            buf[(*index)++] = va_arg(argptr, int);
+            buf[output->index_buf_mass++] = va_arg(output->argptr, int);
             break;
         case 's':
-            char* str2 = va_arg(argptr,  char*);
+            char* str2 = va_arg(output->argptr,  char*);
             buf = strcat(buf,str2);
-            (*index) +=strlen(str2);
+            (output->index_buf_mass) +=strlen(str2);
             break;
         case 'f':
             char str3[20];
-            itoa(va_arg(argptr, int), str3);
+            itoa(va_arg(output->argptr, double), str3, 1);
             buf = strcat(buf,str3);
-            (*index) +=strlen(str3);
+            (output->index_buf_mass) +=strlen(str3);
             break;
         case 'u':
-            char* str4 = va_arg(argptr,  char*);
+            char str4[20];
+            // positiv_int(va_arg(output->argptr, int), str4);
+            itoa(va_arg(output->argptr, unsigned int), str4, 0);
             buf = strcat(buf,str4);
-            (*index) +=strlen(str4);
+            (output->index_buf_mass) +=strlen(str4);
             break;
+        case '%':
+            buf[output->index_buf_mass++] = '%';
+            buf[output->index_buf_mass + 1] = '\0';
+            break;       
         default:
             char out[50];
             s21_sprintf(out, "Флага %c нет", format);
@@ -64,18 +99,45 @@ void sellect_arg(char* buf,int *index, va_list argptr, char format) {
 
 int s21_sprintf(char* buf, char* format, ...)
 {
-    va_list argptr;
-    va_start(argptr, format);
-    int index = 0;
+    write_in_buf output = {0};
+    
+    va_start(output.argptr, format);
     buf[0] = '\0';
+
     int format_len = strlen(format);
-    for(int i=0; i<format_len; i++) {
-        if(format[i] == '%')
-        sellect_arg(buf,&index, argptr, format[++i]);
+    for(output.format_index=0; output.format_index<format_len; output.format_index++) {
+        if(format[output.format_index] == '%')
+        {
+            switch (format[output.format_index+1])
+            {
+            case '-':
+                output.format_index += 2;
+                
+                double space = 0;
+                int i = 0;
+                for(i = 0; format[output.format_index]<='9' && format[output.format_index]>='0';i++)
+                {
+                    space += (format[output.format_index] - '0')/simple_pow(10,i);
+                    output.format_index++;
+                } 
+                space *= simple_pow(10,i-1);
+
+                sellect_arg(buf ,&output, format[output.format_index]);  
+                add_spase(buf,&output, (int)space - output.str_long);
+                break;
+            
+            default:
+                sellect_arg(buf ,&output, format[++output.format_index]);  
+                break;
+            }
+            // if(format[output.format_index+1] == ' ') output.format_index++;
+            // if(format[output.format_index+1] == '+') output.format_index++; 
+        }
+       
         else
-        buf[(index)++] = format[i];
-        buf[index + 1] = '\0';
+        buf[output.index_buf_mass++] = format[output.format_index];
+        buf[output.index_buf_mass + 1] = '\0';
     }
-    va_end(argptr);
+    va_end(output.argptr);
     return 0;
 }
