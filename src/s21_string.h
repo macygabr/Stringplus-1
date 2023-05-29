afa729fe94f3d62cd1253c080480c24d3d0ecafb
Macy Gabrielle <macygabr@student.21-school.ru>
2023-05-29 22:20:20 +0700

    add struct

--- src/s21_string.h
+++ src/s21_string.h
@@ -1,7 +1,23 @@
 #ifndef SRC_S21_STRING_H_
 #define SRC_S21_STRING_H_
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#include <math.h>
+
 int s21_string();
 int s21_sprintf(char* buf, char* format, ...);
+
+typedef struct {
+int index_buf_mass; 
+int format_index;
+int str_long;
+va_list argptr;
+
+} write_in_buf;
 
 #endif
