5c26aafb5d3128259a67381a10fb9695a35f1724
Marsha Ganymede <marshaga@si-b5.nsk.21-school.ru>
2023-06-06 17:27:51 +0700

    2

--- src/test.c
+++ src/test.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 
 int main(void) {
-    char array[7] = "1234567";
+    char array[7] = "asdfghj";
     char *sym = 0;
     printf("%s\n", array);
-    sym = memchr(array, '4', 5);
-    if (sym != NULL) {
-        sym[0] = 'A';
-    }
-    printf("new array is: %s\n", array);
+    sym = memchr(array, '6', 5);
+    sym[0] = 'A';
+    printf("%s\n", array);
     return 0;
 }
+
+// int main() {
+//     FILE *f;
+//     f = fopen("e.txt", "r");
+//     if (f == S21_NULL) {
+//         printf("yes");
+//     } else
+//         printf("don't work");
+//     return 0;
+// }
