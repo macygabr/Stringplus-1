1afdf1bce17a6c6012aa940dd7096d14a7ecbc75
Rodd State <roddstat@ex-h6.nsk.21-school.ru>
2023-06-07 18:28:25 +0700

    h

--- src/fifth.c
+++ src/fifth.c
@@ -1,7 +1,8 @@
+#include "fifth.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 typedef long unsigned s21_size_t;
 
 s21_size_t s21_strlen(const char *str) {
@@ -37,34 +38,10 @@ int main() {
   char *b1 = "ABCD";
   char *b3 = "abc";
   char *buff1 = "12";
-  char *b11 = "1112221132221";
+  char *b11 = "1";
   char *b31;
 
-  // printf("%s to low %s\n", buff, s21_to_lower(buff));
-  // printf("%s to low %s\n", b1, s21_to_lower(b1));
-  // printf("%s to low %s\n", b3, s21_to_lower(b3));
-  // printf("%s to low %s\n", buff1, s21_to_lower(buff1));
-  // printf("%s to low %s\n", b11, s21_to_lower(b11));
-  // printf("%s to low %s\n\n", b31, s21_to_lower(b31));
-  // printf("%s to up %s\n", buff, s21_to_upper(buff));
-  // printf("%s to up %s\n", b1, s21_to_upper(b1));
-  // printf("%s to up %s\n", b3, s21_to_upper(b3));
-  // printf("%s to up %s\n", buff1, s21_to_upper(buff1));
-  // printf("%s to up %s\n", b11, s21_to_upper(b11));
-  // printf("%s to up %s\n\n", b31, s21_to_upper(b31));
-  // char *rez = s21_insert(b3, buff1, 1);
-  // printf("%s in %s from 1 %s\n", b3, buff1, rez);
-  // rez = s21_insert(b3, buff1, 0);
-  // printf("%s in %s from 0 %s\n", b3, buff1, rez);
-  // rez = s21_insert(b3, buff1, 20);
-  // printf("%s in %s from 20 %s\n", b3, buff1, rez);
-  // rez = s21_insert(buff, buff1, 1);
-  // printf("%s in %s from 1 %s\n", buff, buff1, rez);
-  // rez = s21_insert(b3, buff, 0);
-  // printf("%s in %s from 0 %s\n", b3, buff, rez);
-  // rez = s21_insert(buff, buff, 20);
-  // printf("%s in %s from 20 %s\n", buff, buff, rez);
-  printf("%s.\n", s21_trim(b11, buff1));
+  for (int i = 0; i < 300; i++) printf("%s\n", strerror(i));
 
   return 0;
 }
@@ -89,55 +66,29 @@ void *s21_trim(const char *src, const char *trim_chars) {
         end--;
       rezult = (char *)malloc(sizeof(char) * (end - start));
       for (int i = start, j = 0; i <= end; j++, i++) rezult[j] = src[i];
-
-      // 111222113  и шаблоном 12 должно получиться 113
     } else {
       int trim_len = strlen(trim_chars);
-      int start = 0, end = len;
+      int start = 0, end = len - 1;
       char *trim_deleted = malloc(sizeof(trim_chars));
       strcpy(trim_deleted, trim_chars);
       for (int i = 0; i < trim_len; i++) {
-        if (src[start] == trim_deleted[i]) {
-          while (src[start] == trim_deleted[i]) start++;
-          // удалить этот символ из trim_chars
-          char *buff = malloc(sizeof(trim_deleted) - sizeof(char));
-          for (int k = 0, j = 0; j < trim_len; k++, j++) {
-            if (j == i) j++;
-            buff[k] = trim_deleted[j];
-          }
-          free(trim_deleted);
-          trim_deleted = malloc(sizeof(buff));
-          strcpy(trim_deleted, buff);
-          free(buff);
-
-          i = -1;
-          trim_len = trim_len - 1;
+        while (src[start] == trim_deleted[i]) start++;
+        while (src[end] == trim_deleted[i]) end--;
+        char *buff = malloc(sizeof(trim_deleted) - sizeof(char));
+        for (int k = 0, j = 0; j < trim_len; k++, j++) {
+          if (j == i) j++;
+          buff[k] = trim_deleted[j];
         }
+        free(trim_deleted);
+        trim_deleted = malloc(sizeof(buff));
+        strcpy(trim_deleted, buff);
+        free(buff);
+        i = -1;
+        trim_len = trim_len - 1;
       }
       free(trim_deleted);
-      trim_deleted = malloc(sizeof(trim_chars));
-      strcpy(trim_deleted, trim_chars);
-
-      for (int i = 0; i < trim_len; i++) {
-        if (src[end] == trim_deleted[i]) {
-          while (src[end] == trim_deleted[i]) end--;
-          // удалить этот символ из trim_chars
-          char *buff = malloc(sizeof(trim_deleted) - sizeof(char));
-          for (int k = 0, j = 0; j < trim_len; k++, j++) {
-            if (j == i) j++;
-            buff[k] = trim_deleted[j];
-          }
-          free(trim_deleted);
-          trim_deleted = malloc(sizeof(buff));
-          strcpy(trim_deleted, buff);
-          free(buff);
-
-          i = -1;
-          trim_len = trim_len - 1;
-        }
-      }
-      rezult = malloc(sizeof(char) * (end - start));
-      for (int j = 0, i = start; i < end; j++, i++) {
+      rezult = malloc(sizeof(char) * (end - start + 1));
+      for (int j = 0, i = start; i <= end; j++, i++) {
         rezult[j] = src[i];
       }
     }
@@ -190,3 +141,112 @@ void *s21_to_upper(const char *str) {
     rezult = NULL;
   return rezult;
 }
+
+// Undefined error: 0
+// Operation not permitted
+// No such file or directory
+// No such process
+// Interrupted system call
+// Input/output error
+// Device not configured
+// Argument list too long
+// Exec format error
+// Bad file descriptor
+// No child processes
+// Resource deadlock avoided
+// Cannot allocate memory
+// Permission denied
+// Bad address
+// Block device required
+// Resource busy
+// File exists
+// Cross-device link
+// Operation not supported by device
+// Not a directory
+// Is a directory
+// Invalid argument
+// Too many open files in system
+// Too many open files
+// Inappropriate ioctl for device
+// Text file busy
+// File too large
+// No space left on device
+// Illegal seek
+// Read-only file system
+// Too many links
+// Broken pipe
+// Numerical argument out of domain
+// Result too large
+// Resource temporarily unavailable
+// Operation now in progress
+// Operation already in progress
+// Socket operation on non-socket
+// Destination address required
+// Message too long
+// Protocol wrong type for socket
+// Protocol not available
+// Protocol not supported
+// Socket type not supported
+// Operation not supported
+// Protocol family not supported
+// Address family not supported by protocol family
+// Address already in use
+// Can't assign requested address
+// Network is down
+// Network is unreachable
+// Network dropped connection on reset
+// Software caused connection abort
+// Connection reset by peer
+// No buffer space available
+// Socket is already connected
+// Socket is not connected
+// Can't send after socket shutdown
+// Too many references: can't splice
+// Operation timed out
+// Connection refused
+// Too many levels of symbolic links
+// File name too long
+// Host is down
+// No route to host
+// Directory not empty
+// Too many processes
+// Too many users
+// Disc quota exceeded
+// Stale NFS file handle
+// Too many levels of remote in path
+// RPC struct is bad
+// RPC version wrong
+// RPC prog. not avail
+// Program version wrong
+// Bad procedure for program
+// No locks available
+// Function not implemented
+// Inappropriate file type or format
+// Authentication error
+// Need authenticator
+// Device power is off
+// Device error
+// Value too large to be stored in data type
+// Bad executable (or shared library)
+// Bad CPU type in executable
+// Shared library version mismatch
+// Malformed Mach-o file
+// Operation canceled
+// Identifier removed
+// No message of desired type
+// Illegal byte sequence
+// Attribute not found
+// Bad message
+// EMULTIHOP (Reserved)
+// No message available on STREAM
+// ENOLINK (Reserved)
+// No STREAM resources
+// Not a STREAM
+// Protocol error
+// STREAM ioctl timeout
+// Operation not supported on socket
+// Policy not found
+// State not recoverable
+// Previous owner died
+// Interface output queue is full
+// Unknown error: 107
