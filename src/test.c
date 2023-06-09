d64a199733d57ead7c299d853b6d18ca0732f694
Utherydes Grayson <utherydg@si-a4.nsk.21-school.ru>
2023-06-09 16:32:28 +0700

    commit

--- src/test.c
+++ src/test.c
@@ -5,27 +5,27 @@
 #include "s21_string.h"
 
 START_TEST(tests_memchr) {
-static char str1 = "Lorem ipsum dolor sit amet.";
-static char str2 = "12345";
-int sym1 = 115;
-int sym2 = 120;
-int sym3 = 'd';
-int sym4 = 111;
-int sym5 = ' ';
-int sym6 = '4';
-int sym7 = '9';
-s21_size_t len1 = strlen(str1);
-s21_size_t len2 = strlen(str2);
-s21_size_t len5 = 5;
-ck_assert_ptr_eq(memchr(str1, sym1, len1), s21_memchr(str1, sym1, len1)); // поиск символа в полной строке
-ck_assert_ptr_eq(memchr(str2, sym6, len2), s21_memchr(str2, sym6, len2));
-ck_assert_ptr_eq(memchr(str1, sym2, len1), s21_memchr(str1, sym2, len1)); // поиск несуществующего символа в строке
-ck_assert_ptr_eq(memchr(str2, sym7, len2), s21_memchr(str2, sym7, len2));
-ck_assert_ptr_eq(memchr(str1, sym1, len5), s21_memchr(str1, sym1, len5)); // поиск существующего символ, но он находится за пределами сравнения
-ck_assert_ptr_eq(memchr(str1, sym3, len1), s21_memchr(str1, sym3, len1)); // поиск char символа, входящего в строку
-ck_assert_ptr_eq(memchr(str2, sym3, len2), s21_memchr(str2, sym3, len2));
-ck_assert_ptr_eq(memchr(str1, sym3, len1), s21_memchr(str1, sym3, len1)); // проверка на символ, который несколько раз встречается в строке
-ck_assert_ptr_eq(memchr(str1, sym5, len1), s21_memchr(str1, sym5, len1)); // проверка на символ "пробел"
+    static char str1 = "Lorem ipsum dolor sit amet.";
+    static char str2 = "12345";
+    int sym1 = 115;
+    int sym2 = 120;
+    int sym3 = 'd';
+    int sym4 = 111;
+    int sym5 = ' ';
+    int sym6 = '4';
+    int sym7 = '9';
+    s21_size_t len1 = strlen(str1);
+    s21_size_t len2 = strlen(str2);
+    s21_size_t len5 = 5;
+    ck_assert_ptr_eq(memchr(str1, sym1, len1), s21_memchr(str1, sym1, len1)); // поиск символа в полной строке
+    ck_assert_ptr_eq(memchr(str2, sym6, len2), s21_memchr(str2, sym6, len2));
+    ck_assert_ptr_eq(memchr(str1, sym2, len1), s21_memchr(str1, sym2, len1)); // поиск несуществующего символа в строке
+    ck_assert_ptr_eq(memchr(str2, sym7, len2), s21_memchr(str2, sym7, len2));
+    ck_assert_ptr_eq(memchr(str1, sym1, len5), s21_memchr(str1, sym1, len5)); // поиск существующего символ, но он находится за пределами сравнения
+    ck_assert_ptr_eq(memchr(str1, sym3, len1), s21_memchr(str1, sym3, len1)); // поиск char символа, входящего в строку
+    ck_assert_ptr_eq(memchr(str2, sym3, len2), s21_memchr(str2, sym3, len2));
+    ck_assert_ptr_eq(memchr(str1, sym3, len1), s21_memchr(str1, sym3, len1)); // проверка на символ, который несколько раз встречается в строке
+    ck_assert_ptr_eq(memchr(str1, sym5, len1), s21_memchr(str1, sym5, len1)); // проверка на символ "пробел"
 }
 END_TEST
 
@@ -75,32 +75,12 @@ START_TEST(tests_memcpy) {
 }
 END_TEST
 
-Suite *test_memcpy(void) {
-  Suite *s = suite_create("\033[45m-=S21_MEMCPY=-\033[0m");
-  TCase *tc = tcase_create("memcpy_tc");
-
-<<<<<<< HEAD
-  tcase_add_test(tc, tests_memchr);
-  tcase_add_test(tc, tests_memcpy);
-  tcase_add_test(tc, memcpy_3);
-  tcase_add_test(tc, memcpy_4);
-  tcase_add_test(tc, memcpy_5);
-  tcase_add_test(tc, memcpy_6);
-  tcase_add_test(tc, memcpy_7);
-  tcase_add_test(tc, memcpy_8);
-  tcase_add_test(tc, memcpy_9);
-  tcase_add_test(tc, memcpy_10);
-
-  suite_add_tcase(s, tc);
-  return s;
-}
-=======
 int reduction(int arr1, int arr2, int length) {
         int res1 = memcmp(arr1, arr2, length);
         res1 = res1 > 0 ? 1 : res1 == 0 ? 0 : -1;
         return res1;
 }
-START_TEST(test_memcmp) {
+START_TEST(tests_memcmp) {
     s21_size_t len0 = 0;
     s21_size_t len1 = 1;
     s21_size_t len2 = 2;
@@ -247,4 +227,19 @@ START_TEST(test_strncmp) {
     ck_assert_ptr_eq(reduction2(empty_str3, empty_str4, len1), s21_strncmp(empty_str3, empty_str4, len1));
 }
 END_TEST
->>>>>>> a5d7da544c008f65b16125c739be63a7c087d903
+
+
+Suite *test_memchr(void) {
+  Suite *s = suite_create("\033[45m-=S21_MEMCHR=-\033[0m");
+  TCase *tc = tcase_create("memchr_tc");
+
+  suite_add_tcase(s, tc);
+  tcase_add_test(tc, tests_memchr);
+  tcase_add_test(tc, tests_memcpy);
+  tcase_add_test(tc, tests_memcmp);
+  tcase_add_test(tc, tests_memset);
+  tcase_add_test(tc, tests_strchr);
+  tcase_add_test(tc, tests_strrchr);
+  suite_add_tcase(s, tc);
+  return s;
+}
