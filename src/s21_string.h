047b27e6c878caf0922af9db5855a7d36c4aaf47
Marsha Ganymede <marshaga@ob-d8.nsk.21-school.ru>
2023-06-11 12:09:29 +0700

    Add all

--- src/s21_string.h
+++ src/s21_string.h
@@ -97,7 +97,7 @@ s21_size_t s21_strcspn(const char *str1, const char *str2);
 char *s21_strerror(int errnum);
 
 void s21_itoa(int a, char s[]);
-void s21_reverse(char *s);
+
 char *s21_strcat(char *dest, const char *src);
 
 /// @brief Computes the length of the string str up to, but not including the terminating null character.
